a213656004a64cc3e038f52e3e2e0926e0823eb3
Elmar Hinz <t3elmar@googlemail.com>
2013-03-13 22:55:41 +0100

    Adding early classes and tests

---
+++ Modules/Cool/Classes/Manager.php
@@ -0,0 +1,31 @@
+<?php namespace Cool;
+
+class Manager {
+
+	private $classRegister = array();
+
+	public function __construct() {
+		$this->fillClassRegister();
+	}
+
+	public function getClassRegister() {
+		return $this->classRegister;
+	}
+
+	public function fillClassRegister() {
+		$this->classRegister = array();
+		$classes = get_declared_classes();
+		foreach($classes as $class) {
+			foreach(class_implements($class) as $interface) {
+				$this->classRegister[$interface][] = $class;
+			}
+			foreach(class_parents($class) as $interface) {
+				$this->classRegister[$interface][] = $class;
+			}
+			$this->classRegister[$class][] = $class;
+		}
+	}
+
+}
+
+?>
---
+++ Modules/Cool/Tests/IncluderTest.php
@@ -0,0 +1,59 @@
+<?php namespace Cool;
+
+require_once(__DIR__.'/../Classes/Includer.php');
+
+class IncluderTest extends \PHPUnit_Framework_TestCase {
+	
+	private $dir1 = '/tmp/dummy1';
+	private $file1 = '/tmp/dummy1/Dummy1.php';
+	private $dir2 = '/tmp/dummy1/dummy2/';
+	private $file2 = '/tmp/dummy1/dummy2/Dummy2.php';
+
+	function setUp() {
+		mkdir($this->dir1);
+		mkdir($this->dir2);
+		touch($this->file1);
+		touch($this->file2);
+		file_put_contents($this->file1, '<?php $GLOBALS["dummy1"] = 1; ?>');
+		file_put_contents($this->file2, '<?php $GLOBALS["dummy2"] = "two"; ?>');
+	}
+
+	function tearDown() {
+		unlink($this->file2);
+		unlink($this->file1);
+		rmdir($this->dir2);
+		rmdir($this->dir1);
+	}
+
+	/**
+	* @test
+	*/
+	public function includer_can_be_created() {
+		$this->assertInstanceOf('\Cool\Includer', new Includer());
+	}
+
+	/**
+	* @test
+	*/
+	public function pathes_can_be_added() {
+		$includer = new Includer();
+		$includer->addPath($this->dir1);
+		$includer->addPath($this->dir2);
+		$expect = array($this->dir1, $this->dir2);
+		$this->assertEquals($expect, $includer->getPathes());
+	}
+
+	/**
+	* @test
+	*/
+	public function files_are_recursively_included() {
+		$includer = new Includer();
+		$includer->addPath($this->dir1);
+		$includer->run();
+		$this->assertEquals(1, $GLOBALS['dummy1']);
+		$this->assertEquals('two', $GLOBALS['dummy2']);
+	}
+
+}
+
+?>
